1f9618ac1214aed7d570288b7904aa3b95b842ec
toRatnesh <ratneshpublic@gmail.com>
2025-04-10 15:45:45 +0000

    new upadtes

---
+++ books/Scott_Meyers_Effective_Modern_CPP/ch03_Moving_to_Modern_CPP/item16_Make_const_member_functions_thread_safe/thread_safe_const_mem_fun.cpp
@@ -0,0 +1,156 @@
+/*****
+
+References
+
+    Effective Modern C++ | Scott Meyers
+    https://en.cppreference.com/w/cpp/language/member_functions#Member_functions_with_cv-qualifiers
+
+Item 16: Make const member functions thread safe
+
+const member function
+	const member function, represents a read operation
+	Having multiple threads perform a read operation without synchronization is safe
+	
+const member function with mutable member
+	if a mutable member is modified inside a const member function
+	calling that const member function from multiple thread is undefined behavior
+	because multiple threads are reading and writing to mutable member without synchronization
+	
+	The easiest way to address the issue is the usual one: employ a mutex
+	
+const member function and atomic
+	
+	For a single variable or memory location requiring synchronization, use of a std::atomic is adequate, 
+	but once you get to two or more variables or memory locations that require manipulation as a unit, you should reach for a mutex.
+	
+	
+Summary
+• Make const member functions thread safe unless you’re certain they’ll never be used in a concurrent context.
+• Use of std::atomic variables may offer better performance than a mutex, 
+	but they’re suited for manipulation of only a single variable or memory location.
+
+**********/
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <thread>
+#include <atomic>
+#include <mutex>
+
+
+class Point {
+    double  x;
+    double  y;
+
+    mutable uint32_t count = 0;
+    mutable std::atomic<uint32_t> acount{0};
+    
+    public:
+
+    Point(const double x, const double y) : x(x), y(y) { }
+
+    double distaceFromPoint(const Point & p = {0, 0})   {
+        ++count;
+        ++acount;
+        auto d = std::sqrt(((p.x - x) * (p.x - x)) + ((p.y - y) * (p.y - y)));
+        return d;
+    }
+
+    void display() const { std::cout << "Point is {" << x << "," << y << "}\n"; }
+    uint32_t getCount() const { return count; }
+    uint32_t getAtomicCount() const { return acount; }
+};
+
+int expensiveComputation1() { return 1; }
+int expensiveComputation2() { return 2; }
+
+class Widget {
+
+    // for thread safety
+    mutable std::mutex m;
+    mutable int cachedValue{0};                 // no longer atomic
+    mutable bool cacheValid{ false };        // no longer atomic
+
+    // multiple atomic are thread unsafe
+    // mutable std::atomic<bool> cacheValid { false };
+    // mutable std::atomic<int> cachedValue{0};    
+
+public:
+
+    int magicValue() const {
+
+        std::lock_guard<std::mutex> guard(m);
+        
+        if (cacheValid) {
+            std::cout << "Magic value-1: " << cachedValue << '\n';
+            return cachedValue;
+        }
+        else {
+            auto val1 = expensiveComputation1();
+            auto val2 = expensiveComputation2();
+            
+            cachedValue = val1 + val2;
+            cacheValid = true;
+            std::cout << "Magic value-2: " << cachedValue << '\n';
+            return cachedValue;
+        }
+    }
+};
+
+int main() {
+
+    {
+        Point p{3.0, 4.0};
+        p.display();
+        std::cout << p.distaceFromPoint() << '\n';
+    }
+    std::cout << '\n';
+
+    {
+        Point p{3.0, 4.0};
+        Point p2{0.0, 0.0};
+
+        { std::thread t(&Point::distaceFromPoint, std::ref(p), std::cref(p2));     t.detach(); }
+        { std::thread t(&Point::distaceFromPoint, std::ref(p), std::cref(p2));     t.detach(); }
+        { std::thread t(&Point::distaceFromPoint, std::ref(p), std::cref(p2));     t.detach(); }
+        { std::thread t(&Point::distaceFromPoint, std::ref(p), std::cref(p2));     t.detach(); }
+        { std::thread t(&Point::distaceFromPoint, std::ref(p), std::cref(p2));     t.detach(); }
+
+        /*
+        const int thread_count = 100;
+        std::vector<std::thread>    vecth;
+
+        for(int i = 0; i < thread_count; ++i) {
+            vecth.push_back(std::thread(&Point::distaceFromPoint, std::ref(p), std::cref(p2)));
+        }
+
+        for(auto & th : vecth) {
+            th.detach();
+        };
+        */        
+
+        std::cout << "Count " << p.getCount() << '\n';
+        std::cout << "Atomic Count " << p.getAtomicCount() << '\n';
+    }
+    std::cout << '\n';
+
+    {
+        Widget w;
+        { std::thread t(&Widget::magicValue, std::ref(w));     t.detach(); }
+        { std::thread t(&Widget::magicValue, std::ref(w));     t.detach(); }
+        { std::thread t(&Widget::magicValue, std::ref(w));     t.detach(); }
+        { std::thread t(&Widget::magicValue, std::ref(w));     t.detach(); }
+        { std::thread t(&Widget::magicValue, std::ref(w));     t.detach(); }
+
+    }
+
+
+    return 0;
+}
+
+/*****
+    END OF FILE
+**********/
+
+
---
+++ books/Scott_Meyers_Effective_Modern_CPP/ch04_Smart_Pointers/item18_Use_unique_ptr_for_exclusive_ownership_resource_management/using_unique_ptr.cpp
@@ -0,0 +1,170 @@
+/*****
+
+References
+
+    Effective Modern C++ | Scott Meyers
+    https://en.cppreference.com/w/cpp/memory/unique_ptr
+
+Item 18: Use std::unique_ptr for exclusive-ownership resource management
+
+	by default, std::unique_ptrs are the same size as raw pointers, 
+	and for most operations (including dereferencing), they execute exactly the same instructions.
+
+	std::unique_ptr embodies exclusive ownership semantics. 
+	A non-null std::unique_ptr always owns what it points to. 
+	Moving a std::unique_ptr transfers ownership from the source pointer to the destination pointer. (The source pointer is set to null.)
+	std::unique_ptr is thus a move-only type.
+	Upon destruction, a non-null std::unique_ptr destroys its resource.
+
+Deleter
+	By default, that destruction would take place via delete, 
+	but, during construction, std::unique_ptr objects can be configured to use custom deleters: 
+	arbitrary functions (or function objects, including those arising from lambda expressions) 
+	to be invoked when it’s time for their resources to be destroyed.
+
+	All custom deletion functions accept a raw pointer to the object to be destroyed,
+
+Size in case of deleter
+	when using the default deleter std::unique_ptr objects may no longer be the same size as raw pointers
+	function pointers generally cause the size of a std::unique_ptr to grow from one word to two
+	function objects, the change in size depends on how much state is stored in the function object
+	Stateless function objects (e.g., from lambda expressions with no captures) incur no size penalty
+
+	If you find that a custom deleter makes your std::unique_ptrs unacceptably large, you probably need to change your design.
+
+	std::unique_ptrs are even more popular as a mechanism for implementing the Pimpl Idiom.
+
+Variants
+	std::unique_ptr comes in two forms,
+	1.	for individual objects (std::unique_ptr<T>)
+	2.	for arrays (std::unique_ptr<T[]>)
+	there’s no indexing operator (operator[]) for the single-object form, while the array form lacks dereferencing operators (operator* and operator->).
+
+shared pointer using unique pointer
+	std::unique_ptr easily and efficiently converts to a std::shared_ptr
+
+    ------------------------------------------------------------------------------------------------------
+    In C++11 and C++14 it is valid to construct a std::shared_ptr<T> from a std::unique_ptr<T[]>:
+
+    std::unique_ptr<int[]> arr(new int[1]);
+    std::shared_ptr<int> ptr(std::move(arr));
+    Since the shared_ptr obtains its deleter (a std::default_delete<T[]> object) from the std::unique_ptr, the array will be correctly deallocated.
+
+    This is no longer allowed in C++17. Instead the array form std::shared_ptr<T[]> should be used.
+    ------------------------------------------------------------------------------------------------------
+
+Summary
+• std::unique_ptr is a small, fast, move-only smart pointer for managing resources with exclusive-ownership semantics.
+• By default, resource destruction takes place via delete, but custom deleters can be specified. Stateful deleters and function pointers as deleters increase the size of std::unique_ptr objects.
+• Converting a std::unique_ptr to a std::shared_ptr is easy.
+
+**********/
+
+#include <iostream>
+#include <memory>
+
+class St {
+    int     val;
+    public:
+    St(const int ival) : val(ival) { std::cout << "St(const int ival)\n"; }
+    ~St()   { std::cout << "~St()\n"; }
+    operator int() {
+        return val;
+    }
+};
+
+void deleteStFun(St * pst) {
+    if(nullptr != pst) {
+        std::cout << "Deleting St pointer inside deleteStFun\n";
+        delete pst;
+        pst = nullptr;
+    }
+}
+
+auto deleteStLambda = [](St * pst) {
+    if(nullptr != pst) {
+        std::cout << "Deleting St pointer inside deleteStLambda\n";
+        delete pst;
+        pst = nullptr;
+    }
+};
+
+class Wheel {
+    public:
+    virtual ~Wheel() { std::cout << "~Wheel()\n"; }
+};
+
+class AlloyWheel : public Wheel {
+    public:
+    ~AlloyWheel() { std::cout << "~AlloyWheel()\n"; }
+};
+
+class SteelWheel : public Wheel {
+    public:
+    ~SteelWheel() { std::cout << "~SteelWheel()\n"; }
+};
+
+auto make_wheel(const int val = 0) {
+    std::unique_ptr<Wheel> lp{nullptr};
+    if(1== val) {
+        lp.reset(new AlloyWheel{});
+    }
+    else if(2 == val) {
+        lp.reset(new SteelWheel{});
+    }
+    return lp;
+}
+
+int main() {
+    {   std::cout << "=== Using raw pointer ===\n";
+        int * ip = new int{5};
+        std::cout << "Deref pointer, value is " << *ip << '\n';
+        delete ip; ip = nullptr;
+    }
+
+    {   std::cout << "=== Using unique pointer ===\n";
+        std::unique_ptr<int> ip{ new int{5}};
+        std::cout << "Deref pointer, value is " << *ip << '\n';
+    }
+
+    {   std::cout << "=== Using default deleter ===\n";
+        std::unique_ptr<St> lp{ new St{3} };
+        std::cout << "Deref pointer, value is " << *lp << '\n';
+
+        std::cout << "Size of unique pointer is " << sizeof(lp) << '\n';
+    }
+
+    {   std::cout << "=== Using lambda as custom deleter ===\n";
+        std::unique_ptr<St, decltype(deleteStLambda)> lp{ new St{3}, deleteStLambda};
+        std::cout << "Deref pointer, value is " << *lp << '\n';
+
+        std::cout << "Size of unique pointer is " << sizeof(lp) << '\n';
+    }
+
+    {   std::cout << "=== Using function pointer as custom deleter ===\n";
+        std::unique_ptr<St, void(*)(St *)> lp{ new St{3}, deleteStFun};
+        std::cout << "Deref pointer, value is " << *lp << '\n';
+
+        std::cout << "Size of unique pointer is " << sizeof(lp) << '\n';
+    }
+
+    {   std::cout << "=== Creating shared pointer from unique pointer ===\n";
+        std::unique_ptr<St> lup{ new St{7}};
+        std::shared_ptr<St> lsp1{std::move(lup)};
+        std::shared_ptr<St> lsp2{lsp1};
+    }
+
+    {   std::cout << "=== Using factory ===\n";
+        { auto lp = make_wheel(1); }
+        { auto lp = make_wheel(2); }
+        { auto lp = make_wheel(); }
+    }
+
+    return 0;
+}
+
+/*****
+    END OF FILE
+**********/
+
+
